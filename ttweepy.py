6d0e68ff7f2d1f90bb33a7f60eaa36f6572747bf
Yashashvi Dave <yashashvidave@gmail.com>
2019-06-17 17:34:56 +0530

    Add extended version of tweepy.

---
+++ ttweepy.py
@@ -0,0 +1,6 @@
+# This is extended version of tweepy library to return data from Twitter API
+
+def get_tweets(keyword):
+	# TODO: Fetch tweets of keywords from Twitter API
+	print("Fetching tweets of keyword %s" % (keyword))
+	return {}
