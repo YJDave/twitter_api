63de98232c02292625d45c04eee60ebbc1ae8d55
Yashashvi Dave <yashashvidave@gmail.com>
2019-06-17 17:35:18 +0530

    Add query API.

--- api.py
+++ api.py
@@ -1,6 +1,9 @@
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
 
+from db_scripts import store_tweets_to_db
+from ttweepy import get_tweets
+
 app = Flask(__name__)
 api = Api(app)
 
@@ -9,7 +12,20 @@ class Hello(Resource):
     def get(self):
         return jsonify({'hello': 'You can query this API in following format'})
 
+class QueryAPI(Resource):
+	# TODO: Return tweets here match with keyword
+	def get(self, keyword):
+		store_tweets_to_db()
+		tweets = get_tweets(keyword)
+		return jsonify({
+			"result": "List of all tweets with metadata",
+			"tweets": tweets
+		})
+
+
 api.add_resource(Hello, '/')
+api.add_resource(QueryAPI, '/query/<string:keyword>')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
