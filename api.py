52f13ab293a3c8a1680ddf5ec48800a059feaa60
Yashashvi Dave <yashashvidave@gmail.com>
2019-06-18 11:25:39 +0530

    Make configuration class.

--- api.py
+++ api.py
@@ -6,21 +6,8 @@ import os
 import db_scripts
 from tweepy_func import get_tweets
 
-class DatabaseConfig(object):
-    # Database configuration
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-class CeleryConfig(object):
-    # Celery configuration
-    CELERY_BROKER_URL = 'amqp://guest:guest@localhost:5672//'
-    CELERY_BACKEND = os.getenv('DATABASE_URL')
-    CELERY_TASK_SERIALIZER = 'json'
-    CELERY_RESULT_SERIALIZER = 'json'
-
 app = Flask(__name__)
-app.config.from_object(DatabaseConfig)
-app.config.from_object(CeleryConfig)
+app.config.from_object("config.Config")
 api = Api(app)
 db = SQLAlchemy(app)
 
---
+++ config.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+class Config:
+    # Database configuration
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Celery configuration
+    CELERY_BROKER_URL = 'amqp://guest:guest@localhost:5672//'
+    CELERY_BACKEND = os.getenv('DATABASE_URL')
+    CELERY_TASK_SERIALIZER = 'json'
+    CELERY_RESULT_SERIALIZER = 'json'
+
